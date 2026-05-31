14603ec5112ce974fd36bedc85c0239c6b154b80
Joaquin <joasabolo@gmail.com>
2026-05-31 15:25:19 -0300

    Estructura de carpetas y componentes NavBar & Footer

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'wallet',
+      component: () => import('../views/WalletView.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/buy',
+      name: 'buy',
+      component: () => import('../views/BuyView.vue'),
+    },
+    {
+      path: '/sell',
+      name: 'sell',
+      component: () => import('../views/SellView.vue'),
+    },
+    {
+      path: '/transactions',
+      name: 'transactions',
+      component: () => import('../views/TransactionsView.vue'),
     },
   ],
 })
